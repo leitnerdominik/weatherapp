054bfb25b12b6ecde1df06b44256d5d118957bf8
Dominik Leitner <bigtoasti@gmail.com>
2018-10-23 22:34:08 +0200

    detailed weather page, forecast for each hour

--- src/components/DetailedWeather/DetailedWeather.js
+++ src/components/DetailedWeather/DetailedWeather.js
@@ -1,21 +1,77 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import ReactChartkick, { AreaChart } from 'react-chartkick';
+import Chart from 'chart.js';
+import Grid from '@material-ui/core/Grid';
 
+import WeatherType from './WeatherType/WeatherType';
+import { mapToWeatherJson } from '../../shared/util';
 import * as actions from '../../store/actions/weather';
+
+import classes from './DetailedWeather.css';
 
 class DetailedWeather extends Component {
   componentDidMount() {
     const { onFetchDayWeather } = this.props;
     const { match: { params: { date } } } = this.props;
     onFetchDayWeather(date);
+
+    ReactChartkick.addAdapter(Chart);
   }
 
   render() {
+    const {
+      dayTempChart, city, country, hourlyWeather, date,
+    } = this.props;
     return (
-      <h1>Hello World</h1>
+      <div className={classes.Container}>
+        <div className={classes.Title}>
+          <h1>
+            {city}
+            { ' - ' }
+            {country}
+          </h1>
+          <h1 className={classes.CurrentDate}>{date}</h1>
+        </div>
+        <Grid container spacing={8}>
+          <Grid item xs={12}>
+            <Grid container spacing={8} justify="center">
+              {hourlyWeather.map((item) => {
+                const descriptionObj = mapToWeatherJson(item.iconId);
+                return (
+                  <WeatherType
+                    key={item.key}
+                    hour={item.hour}
+                    description={descriptionObj}
+                    humidity={item.humidity}
+                    windSpeed={item.windSpeed}
+                  />
+                );
+              })}
+            </Grid>
+          </Grid>
+        </Grid>
+        <div>
+          <AreaChart
+            messages={{ empty: 'No data' }}
+            ytitle="Temperature"
+            data={dayTempChart}
+          />
+        </div>
+      </div>
     );
   }
 }
+
+const mapStateToProps = state => (
+  {
+    city: state.city,
+    country: state.country,
+    dayTempChart: state.dayTempChart,
+    hourlyWeather: state.hourlyWeather,
+    date: state.currentDate,
+  }
+);
 
 const mapDispatchToProps = dispatch => (
   {
@@ -23,4 +79,4 @@ const mapDispatchToProps = dispatch => (
   }
 );
 
-export default connect(null, mapDispatchToProps)(DetailedWeather);
+export default connect(mapStateToProps, mapDispatchToProps)(DetailedWeather);
