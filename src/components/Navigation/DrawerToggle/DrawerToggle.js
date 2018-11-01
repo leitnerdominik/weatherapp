946644b2c07950617cee59a0d2b1ee55701b798c
Dominik Leitner <bigtoasti@gmail.com>
2018-11-01 22:32:34 +0100

    added a little animation

--- src/components/Navigation/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/DrawerToggle/DrawerToggle.js
@@ -3,12 +3,24 @@ import React from 'react';
 import classes from './DrawerToggle.css';
 
 const drawerToggle = (props) => {
-  const { clicked } = props;
+  const { clicked, sideDrawerOpen } = props;
+
+  let hamburgerMenu = classes.DrawerToggle;
+  if (sideDrawerOpen) {
+    hamburgerMenu = classes.ShowClose;
+  }
   return (
-    <div onClick={clicked} className={classes.DrawerToggle}>
-      <div />
-      <div />
-      <div />
+    // <div onClick={clicked} className={classes.DrawerToggle}>
+    //   <div />
+    //   <div />
+    //   <div />
+    // </div>
+    <div className={classes.DrawerToggleContainer}>
+      <button type="submit" onClick={clicked} className={hamburgerMenu}>
+        <div />
+        <div />
+        <div />
+      </button>
     </div>
   );
 };
